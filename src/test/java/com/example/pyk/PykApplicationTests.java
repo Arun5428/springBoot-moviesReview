e56bce56adec51584cc12be7a5c82381cbd0d6d2
arun <arunkumarkpm69@gmail.com>
2023-07-30 22:01:27 +0530

    files are added

---
+++ src/test/java/com/example/pyk/PykApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pyk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PykApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
